000860fbec442418ed261b4dc2f6d62ba89ae947
OlegMolostov <OlegMolostov>
2020-10-03 03:57:18 +0400

    ТЗ

---
+++ src/Roman.java
@@ -0,0 +1,44 @@
+public enum Roman {
+    I(1),
+    II(2),
+    III(3),
+    IV(4),
+    V(5),
+    VI(6),
+    VII(7),
+    VIII(8),
+    IX(9),
+    X(10),
+    XL(40),
+    L(50),
+    XC(90),
+    C(100);
+
+    private int arab;
+
+    public int getArab() {
+        return arab;
+    }
+
+
+    Roman(int arab) {
+        this.arab = arab;
+    }
+
+    public static String convert(int s) {// перевод из римских -мы идём с конца и приписываем справа цифру,уменьшая арабскую запись
+        Roman[] all = Roman.values();
+        int n = all.length - 1;
+        String S = "";
+        while (s != 0) {
+            if (s == all[n].getArab()) {
+                S += all[n].name();
+                break;
+            } else if (s > all[n - 1].getArab() && n > 0) {
+                S += all[n - 1].name();
+                s -= all[n - 1].getArab();
+            } else n--;
+
+        }
+        return S;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,116 @@
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string;
+
+
+        while (true) {
+            try {
+                System.out.println("Ведите выражение");// По условию нужно было через пробел вводить
+                string = scanner.nextLine();
+                String[] list = string.split(" ");
+                if (list.length == 3 && contains(list[1])) { // проверка что 2 числа и знак, который нам нужен
+                    switch (ArAndRom(list[0], list[2])) {//  проверка на сложение разных типов, а так же размер чисел
+
+                        case 0:
+                            System.out.println("Ответ:");
+                            System.out.println(Operation(list));// функция операций с арабскими числами
+                            break;
+                        case 1:
+                            System.out.println("Ответ:");
+                            System.out.println(Operation1(list));// функция операций с римскими числами(внутри него
+                            break;                                //сначала перевожу в арабские и вызываю предыдущий метод
+
+                        case 2:
+                            throw new Exception("Неверно введены данные!");
+
+
+                    }
+                } else {
+                    throw new Exception("Неверно введены данные!");
+
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
+            }
+
+
+        }
+
+    }
+
+    public static boolean contains(String s) {
+        boolean b = false;
+        String[] operations = {"+", "-", "*", "/"};
+        for (String l : operations
+        ) {
+            if (l.equals(s)) {
+                b = true;
+                break;
+
+            }
+        }
+
+        return b;
+    }
+
+    public static int ArAndRom(String s1, String s2) {
+        int i1, i2;
+        try {
+            i1 = Integer.parseInt(s1);
+            i2 = Integer.parseInt(s2);
+            if (i1 > 0 && i1 < 11 && i2 > 0 && i2 < 11) {
+                return 0;
+            }
+            return 2;
+        } catch (Exception e) {
+            try {
+                Roman r1 = Roman.valueOf(s1);
+                Roman r2 = Roman.valueOf(s2);
+                if (r1.getArab() > 0 && r1.getArab() < 11 && r2.getArab() > 0 && r2.getArab() < 11) {
+                    return 1;
+                }
+                return 2;
+            } catch (Exception l) {
+                return 2;
+            }
+        }
+
+    }
+
+    private static int Operation(String[] s) {
+        int i1, i2;
+        i1 = Integer.parseInt(s[0]);
+        i2 = Integer.parseInt(s[2]);
+
+        if (s[1].equals("+")) {
+            return i1 + i2;
+        } else if (s[1].equals("-")) {
+            return i1 - i2;
+        } else if (s[1].equals("*")) return i1 * i2;
+
+        else return i1 / i2;
+
+    }
+
+    private static String Operation1(String[] s) {
+        String k = "";
+        int res;
+        String[] list = {Roman.valueOf(s[0]).getArab() + "", s[1], Roman.valueOf(s[2]).getArab() + ""};
+        res = Operation(list);
+        k = Roman.convert(res);
+        return k;
+    }
+
+
+}
+
+
+
+
