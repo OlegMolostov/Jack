5ddb1acdce769464fa2906564c540c2f773f129e
OlegMolostov <OlegMolostov>
2020-10-05 01:39:45 +0400

    ТЗ

--- src/Test.java
+++ src/Test.java
@@ -6,15 +6,27 @@ import static methods.Methods.*;
 public class Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("[+*/-]");
         String string;
 
 
         while (true) {
             try {
-                System.out.println("Ведите выражение");// По условию нужно было через пробел вводить
+                System.out.println("Ведите выражение");
                 string = scanner.nextLine();
-                String[] list = string.split(" ");
-                if (list.length == 3 && contains(list[1])) { // проверка что 2 числа и знак, который нам нужен
+                String[] listNumber = string.split("[\\s+*/-]+");//массив чисел
+                String[] listOperation=string.split("[\\sa-zA-Z0-9]+");// масив оператора
+                String[] list=new String[3];//общий
+                try {
+                    list[0]=listNumber[0];
+                    list[1]=listOperation[1];
+                    list[2]=listNumber[1];
+                }
+                catch (Exception k){
+                    throw new Exception("Неверно введены данные!");
+                }
+
+                if (contains(list[1])) { // проверка что 2 числа и знак, который нам нужен
                     switch (ArAndRom(list[0], list[2])) {//  проверка на сложение разных типов, а так же размер чисел
 
                         case 0:
